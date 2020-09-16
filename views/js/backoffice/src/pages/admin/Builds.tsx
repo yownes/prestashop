39e1a2e9ae0d6307d70eeb29a6d757bd01e15e47
Jesús Gallego Irles <jesusgallego@outlook.com>
2020-09-16 15:39:12 +0200

    Builds and clients tables

--- views/js/backoffice/src/pages/admin/Builds.tsx
+++ views/js/backoffice/src/pages/admin/Builds.tsx
@@ -1,9 +1,144 @@
 import React from "react";
+import { Table, Tag } from "antd";
+import { ColumnsType } from "antd/lib/table";
 
-interface BuildsProps {}
+interface ClientType {
+  id: string;
+  name: string;
+}
 
-const Builds = ({}: BuildsProps) => {
-  return <div>Builds</div>;
+interface AppType {
+  name: string;
+  client: ClientType;
+}
+
+enum BuildState {
+  STALLED = "STALLED",
+  QUEUED = "QUEUED",
+  GENERATING = "GENERATING",
+  UPLOADING = "UPLOADING",
+  PUBLISHED = "PUBLISHED",
+  WAITING = "WAITING",
+}
+
+interface BuildType {
+  id: string;
+  date: Date;
+  app: AppType;
+  state: BuildState;
+}
+
+const COLORS = {
+  STALLED: "default",
+  QUEUED: "gold",
+  GENERATING: "cyan",
+  UPLOADING: "magenta",
+  PUBLISHED: "green",
+  WAITING: "orange",
+};
+
+const columns: ColumnsType<BuildType> = [
+  {
+    title: "Fecha",
+    dataIndex: "date",
+    key: "data",
+    render: (date: Date) => date.toLocaleDateString(),
+  },
+  { title: "ID build", dataIndex: "id", key: "buildId" },
+  { title: "Cliente", dataIndex: ["app", "client", "name"], key: "client" },
+  { title: "ID Cliente", dataIndex: ["app", "client", "id"], key: "clientId" },
+  { title: "App", dataIndex: ["app", "name"], key: "app" },
+  {
+    title: "Estado",
+    dataIndex: "state",
+    key: "state",
+    render: (state: BuildState) => {
+      const color = COLORS[state];
+      return <Tag color={color}>{state}</Tag>;
+    },
+  },
+];
+
+const Builds = () => {
+  const data: BuildType[] = [
+    {
+      id: "1",
+      date: new Date(),
+      app: {
+        name: "App1",
+        client: {
+          id: "sdrt789hd1qegq823r23f",
+          name: "Alejandro",
+        },
+      },
+      state: BuildState.STALLED,
+    },
+    {
+      id: "2",
+      date: new Date(),
+      app: {
+        name: "App3",
+        client: {
+          id: "fgvewargbftrn4532",
+          name: "Jesús",
+        },
+      },
+      state: BuildState.QUEUED,
+    },
+    {
+      id: "3",
+      date: new Date(),
+      app: {
+        name: "App3",
+        client: {
+          id: "fgvewargbftrn4532",
+          name: "Jesús",
+        },
+      },
+      state: BuildState.GENERATING,
+    },
+    {
+      id: "4",
+      date: new Date(),
+      app: {
+        name: "App3",
+        client: {
+          id: "fgvewargbftrn4532",
+          name: "Jesús",
+        },
+      },
+      state: BuildState.UPLOADING,
+    },
+    {
+      id: "5",
+      date: new Date(),
+      app: {
+        name: "App3",
+        client: {
+          id: "fgvewargbftrn4532",
+          name: "Jesús",
+        },
+      },
+      state: BuildState.WAITING,
+    },
+    {
+      id: "6",
+      date: new Date(),
+      app: {
+        name: "App3",
+        client: {
+          id: "fgvewargbftrn4532",
+          name: "Jesús",
+        },
+      },
+      state: BuildState.PUBLISHED,
+    },
+  ];
+  return (
+    <div>
+      <Table columns={columns} dataSource={data} />
+    </div>
+  );
 };
 
 export default Builds;
--- views/js/backoffice/src/pages/admin/Clients.tsx
+++ views/js/backoffice/src/pages/admin/Clients.tsx
@@ -1,9 +1,88 @@
+import { Tag } from "antd";
+import Table, { ColumnsType } from "antd/lib/table";
 import React from "react";
 
-interface ClientsProps {}
+enum AccountState {
+  STALLED = "STALLED",
+  CONFIRMING = "CONFIRMING",
+  PAID = "PAID",
+  BANNED = "BANNED",
+}
 
-const Clients = ({}: ClientsProps) => {
-  return <div>Clients</div>;
+interface AppType {
+  id: string;
+}
+
+interface ClientType {
+  id: string;
+  state: AccountState;
+  name: string;
+  apps: AppType[];
+}
+
+const COLORS = {
+  STALLED: "default",
+  CONFIRMING: "orange",
+  PAID: "green",
+  BANNED: "red",
+};
+
+const columns: ColumnsType<ClientType> = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  { title: "ID Cliente", dataIndex: "id", key: "id" },
+  {
+    title: "Apps",
+    dataIndex: "apps",
+    key: "apps",
+    render: (apps: AppType[]) => apps.length,
+  },
+  {
+    title: "Estado",
+    dataIndex: "state",
+    key: "state",
+    render: (state: AccountState) => {
+      const color = COLORS[state];
+      return <Tag color={color}>{state}</Tag>;
+    },
+  },
+];
+
+const Clients = () => {
+  const data: ClientType[] = [
+    {
+      id: "bvxdtyq56w7w89",
+      name: "Jesús",
+      apps: [{ id: "1" }],
+      state: AccountState.STALLED,
+    },
+    {
+      id: "bvxdtyq56w7w89",
+      name: "Alejandro",
+      apps: [{ id: "1" }, { id: "2" }, { id: "3" }],
+      state: AccountState.CONFIRMING,
+    },
+    {
+      id: "bvxdtyq56w7w89",
+      name: "Antoni",
+      apps: [{ id: "1" }, { id: "1" }],
+      state: AccountState.PAID,
+    },
+    {
+      id: "bvxdtyq56w7w89",
+      name: "Jorge",
+      apps: [{ id: "1" }],
+      state: AccountState.BANNED,
+    },
+  ];
+  return (
+    <div>
+      <Table columns={columns} dataSource={data} />
+    </div>
+  );
 };
 
 export default Clients;
