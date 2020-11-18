efcca6f0667897f7b88f82e591013c0c68126af9
Jesús Gallego Irles <jesusgallego@outlook.com>
2020-11-18 16:23:56 +0100

    i18n: ToS

--- views/js/backoffice/src/pages/Tos.tsx
+++ views/js/backoffice/src/pages/Tos.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const Tos = () => {
+  const { t } = useTranslation();
   return (
     <div>
-      <h1>Términos y condiciones</h1>
+      <h1>{t("tos.title")}</h1>
     </div>
   );
 };
